a3f7d41e3ca04923119e6a35286defb6f0c2cca2
Jack Ellis <Jack@Administrators-MacBook-Pro.local>
2017-02-19 10:02:45 +0000

    x

--- src/index.js
+++ src/index.js
@@ -3,22 +3,22 @@ var $tick = require('./$tick');
 var nodemodule = require('./nodemodule');
 
 module.exports = {
-  install : function ({Jpex, on}) {
-    if (!Jpex.$$factories.$promise){
-      var defaults = require('jpex-defaults');
-      Jpex.use(defaults);
+    install : function ({Jpex, on}) {
+        if (!Jpex.$$factories.$promise){
+            var defaults = require('jpex-defaults');
+            Jpex.use(defaults);
+        }
+
+        Jpex.register.factory('$fs', ['fs', '$promise'], $fs).lifecycle.class();
+
+        Jpex.register.factory('$tick', [], $tick).lifecycle.application();
+
+        Jpex.register.node_module = nodemodule.bind(Jpex);
+
+        on('factories', function ({register}) {
+            register('node_module', nodemodule);
+        });
     }
-
-    Jpex.register.factory('$fs', ['fs', '$promise'], $fs).lifecycle.class();
-
-    Jpex.register.factory('$tick', [], $tick).lifecycle.application();
-
-    Jpex.register.node_module = nodemodule.bind(Jpex);
-
-    on('factories', function ({register}) {
-      register('node_module', nodemodule);
-    });
-  }
 };
 
 if (typeof window !== 'undefined' && window.Jpex && typeof window.Jpex.use === 'function'){
